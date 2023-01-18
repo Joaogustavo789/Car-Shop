0f5409c1407393236ba4859409073590a9d11ae6
Joao Gustavo <jgustavomendonca@gmail.com>
2023-01-18 12:25:05 -0300

    Requisito 6: 60% de cobertura da Service

--- tests/unit/Services/cars.service.test.ts
+++ tests/unit/Services/cars.service.test.ts
@@ -5,7 +5,33 @@ import ICar from '../../../src/Interfaces/ICar';
 import Car from '../../../src/Domains/Car';
 import CarsService from '../../../src/Services/cars.service';
 
+const carsArray = [
+  {
+    id: '63c75d7583a096c16fa5df11',
+    model: 'Marea',
+    year: 2003,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+];
+
 describe('Testes de cars.', function () {
+  it('Testa se lista todos os carros', async function () {
+    const carsOutput2 = carsArray.map((car) => new Car({ ...car }));
+
+    sinon.stub(Model, 'find').resolves(carsOutput2);
+
+    const service2 = new CarsService();
+    const result2 = await service2.getCarsService();
+
+    expect(result2).to.be.deep.equal(carsArray);
+
+    sinon.restore();
+  });
+
   it('Testa se cadastra um novo carro', async function () {
     const newCar = {
       model: 'Marea',
@@ -35,6 +61,10 @@ describe('Testes de cars.', function () {
 
     expect(result).to.be.deep.equal(carOutput);
   });
+
+  // it('Testa se atualiza os carros', function () {
+
+  // });
 
   afterEach(function () {
     sinon.restore();
--- tests/unit/Services/motorcycle.service.test.ts
+++ tests/unit/Services/motorcycle.service.test.ts
@@ -5,10 +5,38 @@ import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
 import Motorcycle from '../../../src/Domains/Motorcycle';
 import MotorService from '../../../src/Services/motorcycles.service';
 
+const honda = 'Honda Cb 600f Hornet';
+
+const motorArray = [
+  {
+    id: '63c80e78eeec10a1f841945a',
+    model: honda,
+    year: 2005,
+    color: 'Yellow',
+    status: true,
+    buyValue: 30,
+    category: 'Street',
+    engineCapacity: 600,
+  },
+];
+
 describe('Testes de motorcycles.', function () {
+  it('Testa se lista todas as motos', async function () {
+    const motorOutput2 = motorArray.map((motor) => new Motorcycle({ ...motor }));
+
+    sinon.stub(Model, 'find').resolves(motorOutput2);
+
+    const service2 = new MotorService();
+    const result2 = await service2.getMotorService();
+
+    expect(result2).to.be.deep.equal(motorArray);
+
+    sinon.restore();
+  });
+
   it('Testa se cadastra uma nova moto', async function () {
     const newMotor = {
-      model: 'Honda Cb 600f Hornet',
+      model: honda,
       year: 2005,
       color: 'Yellow',
       status: true,
@@ -18,7 +46,7 @@ describe('Testes de motorcycles.', function () {
     };
 
     const motor: IMotorcycle = {
-      model: 'Honda Cb 600f Hornet',
+      model: honda,
       year: 2005,
       color: 'Yellow',
       status: true,
